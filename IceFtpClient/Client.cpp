88099b5a3820a92b66fc1cdfa8d84ee00e45343e
Cathy Lin <cathy.lin1219@hotmail.com>
2015-02-21 22:51:13 +0800

    Base Frame is done. Upload function successfull

--- IceFtpClient/Client.cpp
+++ IceFtpClient/Client.cpp
@@ -6,10 +6,20 @@
 using namespace std;
 using namespace Cathy;
 
+
 class FileTransferClient : public Ice::Application
 {
 public:
+	FileTransferClient();
 	virtual int run(int, char*[]);
+private:
+	const char* menu;
+	FileTransferPrx transferPrx;
+	void showMenu();
+	int parsCommand(const char* strCommand);
+	void Upload(const char* strFilePath);
+	void Download(const char* strFilePath);
+	void GetList();
 };
 
 
@@ -29,51 +39,17 @@ FileTransferClient::run(int argc, char* argv[])
 		ic = Ice::initialize(argc, argv);
 		Ice::ObjectPrx base = ic->stringToProxy("SimpleTransferID:tcp -h localhost -p 7788");
 		//Ice::ObjectPrx base = ic->propertyToProxy("FileStore.Proxy");
-			FileTransferPrx transferPrx = FileTransferPrx::checkedCast(base);
+		transferPrx = FileTransferPrx::checkedCast(base);
 		if (!transferPrx)
 			throw "Invalid proxy";
 
-		//FileHandle file = open(...);
-		string output = "output.txt";
-		FILE *wrfl = fopen(output.c_str(), "w+");
-		fwrite("asdfasd", 1, 7, wrfl);
-		fclose(wrfl);
-		FILE* file = fopen(output.c_str(), "rb");
-		if (file == NULL)             /*判断文件是否打开成功*/
-			cout<<"File open error";/*提示打开不成功*/
-		const int chunkSize = 1000 * 1024;
-		Ice::Int offset = 0;
-
-		list<Ice::AsyncResultPtr> results;
-		const int numRequests = 5;
-
-		while (!feof(file))
-		{
-			ByteSeq bs(chunkSize);
-			fread(&bs[0], 1, chunkSize, file);
-
-			// Send up to numRequests + 1 chunks asynchronously.
-			Ice::AsyncResultPtr r = transferPrx->begin_write(output, offset, bs);//让服务端写，即上传
-			offset += bs.size();
-
-			// Wait until this request has been passed to the transport.
-			r->waitForSent();
-			results.push_back(r);
-
-			// Once there are more than numRequests, wait for the least
-			// recent one to complete.
-			while (results.size() > numRequests) {
-				Ice::AsyncResultPtr r = results.front();
-				results.pop_front();
-				r->waitForCompleted();
-			}
-		}
-
-		// Wait for any remaining requests to complete.
-		while (!results.empty()) {
-			Ice::AsyncResultPtr r = results.front();
-			results.pop_front();
-			r->waitForCompleted();
+		//进入菜单处理部分
+		char command[100];
+		int status = 1;//初始状态为1；若为0，则退出；若为-1，则出错；若为1，则正常处理
+		while (status){
+			showMenu();
+			cin.getline(command, 100);
+			parsCommand(command);
 		}
 	}
 	catch (const Ice::Exception& ex) {
@@ -87,4 +63,129 @@ FileTransferClient::run(int argc, char* argv[])
 	if (ic)
 		ic->destroy();
 	return status;
+}
+
+FileTransferClient::FileTransferClient()
+{
+	menu =	
+		"*****************test ice client *********************** \n"
+		"\tupload [LocalFilePath]\n"
+		"\tdownload [ServerFilePath] [LocalFilePath]\n"
+		"\tlist \n"
+		"\tq\n"
+		"******************************************************** \n";
+}
+
+void FileTransferClient::showMenu()
+{
+	std::cout << menu << std::endl;
+	return;
+}
+
+int FileTransferClient::parsCommand(const char* strCommand)
+{
+	//以下四种命令
+	//upload [LocalFilePath]
+	//download [ServerFilePath] [LocalFilePath]
+	//list 
+	//q
+	char strCommandCopy[100];
+	strcpy(strCommandCopy, strCommand);
+	char element[3][100];
+	int elementNum=0;
+	char *temp = strtok(strCommandCopy, " \t");
+	while (temp != NULL)
+	{
+		if (elementNum == 3){
+			cout << "Command error : element more than three!" << endl;
+			break;
+			//throw "throw command error : element more than three!!";
+		}
+		strcpy(element[elementNum], temp);
+		temp = strtok(NULL, " \t");
+		elementNum++;
+	}
+
+	//处理分割完的命令
+	if (strcmp(element[0], "upload") == 0){
+		//处理 upload [LocalFilePath]
+		Upload(element[1]);
+		return 1;
+	}
+	else if (strcmp(element[0], "download") == 0){
+		//处理 download [ServerFilePath] [LocalFilePath]
+		return 1;
+	}
+	else if (strcmp(element[0], "list") == 0){
+		//处理 list
+		return 1;
+	}
+	else if (strcmp(element[0], "q") == 0){
+		//处理 q
+		return 0;
+	}
+	else{
+		cout<< "unknown command!"<<endl;
+		return -1;
+	}
+
+}
+
+void FileTransferClient::Upload(const char* strFilePath)
+{
+	//string output = "output.txt";
+
+	//FILE *wrfl = fopen(output.c_str(), "w+");
+	//fwrite("asdfasd", 1, 7, wrfl);
+	//fclose(wrfl);
+	
+	//FILE* file = fopen(output.c_str(), "rb");
+	FILE* file = fopen(strFilePath, "rb");
+
+	if (file == NULL)             /*判断文件是否打开成功*/
+		cout << "File open error"<<endl;
+
+	const int chunkSize = 1024;
+	Ice::Int offset = 0;
+
+	list<Ice::AsyncResultPtr> results;
+	const int numRequests = 5;
+
+	//解析文件名
+	string strFileName=strFilePath;	// Remove directory if present.
+	const size_t last_slash_idx = strFileName.find_last_of("\\/");
+	if (std::string::npos != last_slash_idx)
+	{
+		strFileName.erase(0, last_slash_idx + 1);
+	}
+
+	while (!feof(file))
+	{
+		ByteSeq bs(chunkSize);
+		fread(&bs[0], 1, chunkSize, file);
+
+		// Send up to numRequests + 1 chunks asynchronously.
+		Ice::AsyncResultPtr r = transferPrx->begin_write(strFileName, offset, bs);//让服务端写，即上传
+		offset += bs.size();
+
+		// Wait until this request has been passed to the transport.
+		r->waitForSent();
+		results.push_back(r);
+
+		// Once there are more than numRequests, wait for the least
+		// recent one to complete.
+		while (results.size() > numRequests) {
+			Ice::AsyncResultPtr r = results.front();
+			results.pop_front();
+			r->waitForCompleted();
+		}
+	}
+
+	// Wait for any remaining requests to complete.
+	while (!results.empty()) {
+		Ice::AsyncResultPtr r = results.front();
+		results.pop_front();
+		r->waitForCompleted();
+	}
+
 }
